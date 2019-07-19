d3d2a28dfaceccec42aed78e74996256610cbcd5
rubairshaid <rubairshed1@gmail.com>
2019-07-19 17:44:04 +0300

    the begining if the program

---
+++ SNProject/SourceSNProject.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<time.h>
+#include<ctime>
+#pragma warning(disable : 4996).
+using namespace std;
+
+/*time_t rawtime;
+  struct tm * timeinfo;
+  char buffer[80];
+
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
+  std::string str(buffer);
+
+  std::cout << str;
+*/
+
+
+int main()
+{
+	
+
+	
+	system("pause");
+	return 0;
+}
