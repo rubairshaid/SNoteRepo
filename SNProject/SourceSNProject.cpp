f3ceff693cf15853bdcee856881a91694f795900
rubairshaid <rubairshed1@gmail.com>
2019-07-20 00:24:20 +0300

    solving case 1 for interface 2 in the project

--- SNProject/SourceSNProject.cpp
+++ SNProject/SourceSNProject.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<iomanip>
+#include<fstream>
 #include<time.h>
 #include<ctime>
 #pragma warning(disable : 4996).
@@ -21,32 +22,53 @@ using namespace std;
 
 void menu ()
 {
-	cout<<"Welcome to the brand new â€œSticky Notesâ€!"<<endl;
+	cout<<"Welcome to the brand new “Sticky Notes”!"<<endl;
 	cout<<"Here is the list of operation this program offers:"<<endl;
 	cout<<"1- Add new user"<<endl;
-	cout<<"2- Add new note"<<endl;
-        cout<<"3- View notes for a specific user"<<endl;
-        cout<<"4- Exit"<<endl;
-}
+	cout<<"2- Add new note "<<endl;
+    cout<<"3- View notes for a specific user "<<endl;
+    cout<<"4- Exit"<<endl;
 
+
+
+
+}
 int main()
 {
-	int num;
-
+	int num;char ch;
+	string Fname,Lname;
 	menu();
 	cin>>num;
+	ifstream fin;
+	ofstream fout;
 
 	switch(num)
 	{
 	case 1:
+		system("cls");
+		cout <<"Welcome aboard new user!"<<endl;
+		cout<<"Please let me know your first name: ";
+		cin>>Fname;
+		cout<<"Great "<<Fname<<", now please enter your last name: ";
+		cin>>Lname;
+		cout<<"Done!"<<endl;
+		cout<<"Nice to meet you "<<Fname<<" "<<Lname<<endl;
+		cin.get(ch);
+		Fname+=Lname; 
+		fout.open(Fname);
+		fout<<""<<endl;
+		fout.close();
+		cin.get(ch);
+		system("cls");
+		menu();
 		break;
 	case 2:
+
 		break;
 	case 3:
 
 		break;
 	case 4:
-
 
 		break;
 
